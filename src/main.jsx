016cb7a53b331bcc65b0d3f6580ddf9ec242c7dc
rashid-flutter <rashidrashi0004321@gmail.com>
2026-04-29 23:40:33 +0530

    small changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // Must match GitHub repo name
+  base: "/", // Must match GitHub repo name
   build: {
     outDir: "dist",
   },
